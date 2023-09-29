5cfdc0d268e68d0c3fa45210da7b2568a4cbe233
zachbazov <zachbazov@gmail.com>
2023-09-29 15:02:36 +0300

    Refactored package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["URLDataTransfer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/zachbazov/CodeBureau.git", from: "1.0.2")
+        .package(url: "https://github.com/zachbazov/CodeBureau.git", from: "1.0.4")
     ],
     targets: [
         .target(name: "URLDataTransfer", dependencies: [
